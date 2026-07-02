61882061e71d434effbf7a02ee0c98cb5c591ca6
laptop <agubin8234@gmail.com>
2026-07-02 23:09:25 +0300

    дайте мне пистолет ну пожалуйста(я дегенерат btw) final part(или нет)

--- build.sh
+++ build.sh
@@ -7,8 +7,6 @@ pip install -r requirements.txt
 echo "📦 Static"
 python project/manage.py collectstatic --noinput
 
-echo "📋 ONE-TIME: add programs"
-python project/scripts/add_program.py
 
 echo "🗄️ Migrate"
 python project/manage.py migrate --noinput

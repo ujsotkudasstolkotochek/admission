e73cb70f4d9ae21b4832dbf44182f9d6d7581ef4
ujsotkudastolkotochek <160528845+ujsotkudasstolkotochek@users.noreply.github.com>
2026-07-02 16:01:13 +0300

    Refactor start_scheduler function parameters
    Refactor start_scheduler to accept kwargs and remove table existence check.

--- project/parsers/scheduler.py
+++ project/parsers/scheduler.py
@@ -1,24 +1,13 @@
-# parsers/scheduler.py
 import os
 from apscheduler.schedulers.background import BackgroundScheduler
 from django_apscheduler.jobstores import DjangoJobStore
 from .services import update_all_programs
-from django.db import connection
-from django.db.utils import OperationalError
 
 SCHEDULER_ENABLED = os.environ.get('SCHEDULER_ENABLED', 'True') == 'True'
 
-def start_scheduler():
+def start_scheduler(**kwargs):
     if not SCHEDULER_ENABLED:
         print("⏹ Планировщик отключён через переменную окружения")
-        return
-
-    # Проверяем, существует ли таблица для хранения задач
-    try:
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT 1 FROM django_apscheduler_djangojob LIMIT 1")
-    except OperationalError:
-        print("⚠️ Таблица django_apscheduler_djangojob не существует. Планировщик не запущен. Выполните миграции и запустите вручную.")
         return
 
     scheduler = BackgroundScheduler()

6bd9abb14fac8fe4c6a243ec48c08c42463fc978
ujsotkudastolkotochek <160528845+ujsotkudasstolkotochek@users.noreply.github.com>
2026-07-02 14:41:23 +0300

    trying to deploy on server

--- project/core/views.py
+++ project/core/views.py
@@ -11,14 +11,14 @@ def programs_stats_api(request):
     programs = Program.objects.select_related('university').all().order_by('university__name', 'code')
 
     user_score = None
-    user_status = 'common'
+    user_status = 'common'  # статус пользователя по умолчанию
     show_place = False
 
     if request.user.is_authenticated and hasattr(request.user, 'profile'):
         profile = request.user.profile
         if profile.score is not None:
             user_score = profile.score
-            user_status = profile.status
+            user_status = profile.status or 'common'  # если не задан, считаем common
             show_place = True
 
     universities_data = {}
@@ -27,18 +27,13 @@ def programs_stats_api(request):
         if uni_name not in universities_data:
             universities_data[uni_name] = []
 
-        # Расчёт места, только если пользователь авторизован и есть баллы
         if show_place and user_score is not None:
-            above = program.applicants.filter(score__gt=user_score).count()
-            zero = program.applicants.filter(score=0).count()
-            place = above + zero + 1
-
-            # Поправка для ИТМО (расхождение в 4 позиции)
-            if 'itmo.ru' in program.url:
-                place += 3
-
+            # Фильтруем абитуриентов с таким же статусом и баллом > 0
+            applicants = program.applicants.filter(status=user_status, score__gt=0)
+            above = applicants.filter(score__gt=user_score).count()
+            place = above + 1
             program.user_place = place
-            program.user_total = program.applicants.count()
+            program.user_total = applicants.count()
         else:
             program.user_place = None
             program.user_total = 0
